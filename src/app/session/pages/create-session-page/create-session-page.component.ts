e48b288cf736344c1175a73ffc4333837b9f29dc
EduardoSalasG <eduardo.salasgo@correoaiep.cl>
2024-05-18 23:33:10 -0400

    Rutas ok, estructura de header y footer listas

--- src/app/session/pages/create-session-page/create-session-page.component.ts
+++ src/app/session/pages/create-session-page/create-session-page.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-create-session-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive,],
   templateUrl: './create-session-page.component.html',
   styleUrl: './create-session-page.component.css'
 })
 export class CreateSessionPageComponent {
+
+  constructor(private router: Router) { }
+
+  save() {
+    this.router.navigateByUrl('/session/list')
+  }
 
 }
